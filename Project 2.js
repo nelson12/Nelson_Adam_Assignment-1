4e910c949c41362b9ee67539910bea33a3accfd5
nelson12# <aw.nelson@hotmail.com>
2014-05-18 20:13:57 -0400

    declaring variables

---
+++ Project 2.js
@@ -0,0 +1,15 @@
+//Adam Nelson
+//SDI Online
+//05/15/2014
+//Project 2
+
+//Declaring variables
+var game= "World of Warcraft";
+var races= 14;
+var classes=11;
+var loggingIn="Password";
+var yourRace= "Human"; 
+var yourClass= "Hunter";
+var start= 'Let\’s play!';
+var hunter= true;
+
