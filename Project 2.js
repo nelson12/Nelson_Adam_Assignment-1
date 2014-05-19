f7ce2b7d26ad3411fdcc9e4c2654163748c581e2
nelson12# <aw.nelson@hotmail.com>
2014-05-18 20:14:51 -0400

    output

--- Project 2.js
+++ Project 2.js
@@ -13,3 +13,14 @@ var yourClass= "Hunter";
 var start= 'Let\’s play!';
 var hunter= true;
 
+//Output
+console.log(" You are playing " + game + ".");
+console.log(" There are " + races + " races. ");
+console.log(" There are " + classes + " classes. ");
+console.log(" You will be using your " + loggingIn + " to authenticate. ");
+console.log(" You will be a " + yourRace +  yourClass + ".");
+console.log(" Come on " + start + "!"); 
+
+
+
+
